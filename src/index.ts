c363fa7e66921fae0b3701d963a68b13b6ed2856
MadsWJespersen <Mads_1997@live.dk>
2020-09-18 01:19:13 +0200

    before typeorm setup

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import app from './App'
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 80
 
 app.listen(port, (err) =>{
     if (err) {
         return console.log(err)
     }
-    return console.log('server is listening on ${port}')
+    return console.log(`server is listening on ${port}`)
 })
