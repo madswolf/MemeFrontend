1ad394b558b8e4dfc7754e2d2d0eda74091b3b0c
MadsWJespersen <Mads_1997@live.dk>
2020-09-22 22:09:19 +0000

    added https support for backend, and tested serving the react app from the backend

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,20 +3,29 @@ import {createConnection} from "typeorm";
 import * as express from "express";
 import * as logger from 'morgan'
 import * as bodyParser from "body-parser";
+import * as path from 'path';
 import {Request, Response} from "express";
 import {Routes} from "./routes";
 import {User} from "./entity/User";
-
+import * as fs from 'fs';
+import * as http from 'http';
+import * as https from 'https';
 
 createConnection().then(async connection => {
 
     // create express app
-    const app = express();
-    const port = 80;
-    app.use(bodyParser.json());
-    app.use(express.static('public'))
+    const app = express()
+    const port = 3000
+    const pathToCert = "/etc/letsencrypt/live/mads.monster/"
+    app.use(bodyParser.json())
+    app.use(express.static(path.join(__dirname,'build')))
+    app.use(express.static(__dirname, {dotfiles: 'allow'}))
     logger('tiny')
     app.use(logger('dev'))
+
+    app.get('/',function(req,res){
+        res.sendFile(path.join(__dirname,'build','index.html'))
+    })
 
     // register express routes from defined application routes
     Routes.forEach(route => {
@@ -32,21 +41,24 @@ createConnection().then(async connection => {
     });
 
     // setup express app here
-    // ...
-    
-    
-    // start express server
-    app.listen(80);
+    // setup https
+    const privateKey = fs.readFileSync(path.join(pathToCert,'privkey.pem'), 'utf8');
+    const certificate = fs.readFileSync(path.join(pathToCert,'cert.pem'), 'utf8');
+    const ca = fs.readFileSync(path.join(pathToCert,'chain.pem'), 'utf8');
 
-    // insert new users for test
-    await connection.manager.save(connection.manager.create(User, {
-        userName: "Gamer420",
-        passwordHash: "ax987dacksad",
-    }));
-    await connection.manager.save(connection.manager.create(User, {
-        userName: "xxPhantom69xx",
-        passwordHash: "15m,4123axdwq",
-    }));
+    const credentials = {
+	    key: privateKey,
+	    cert: certificate,
+	    ca: ca
+    };
+
+    const httpServer = http.createServer(app);
+    const httpsServer = https.createServer(credentials, app);
+
+    // start express server
+
+    httpServer.listen(port);
+    httpsServer.listen(443);
 
     console.log(`Express server has started on port ${port}. Open http://localhost:${port}/users to see results`);
 
