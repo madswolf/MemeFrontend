c4dc3f1f84764fc48fa3cb86a30b23a235b8b653
oeje <oeje@itu.dk>
2020-10-06 00:45:43 +0200

    not quite sending files from backend to frontend

--- frontend/src/MemePage.tsx
+++ frontend/src/MemePage.tsx
@@ -6,13 +6,20 @@ import { useMemeState } from './State';
 const MemePage :React.FC = (props) =>{
     const canvasRef = React.useRef(null);
     const {toptext,setTopText,bottomtext,setBottomText,visualFile,setVisualFile,soundFile,setSoundFile} = useMemeState();
-    
+    async function test(){
+        const fileReader = new FileReader();
+        fetch('http://localhost:2000/randommeme',{method:'GET'})
+        .then(response => response.json())
+        .then(data => console.log(data));
+    }
     return (
         <div>
             <canvas ref={canvasRef}>
 
             </canvas>
-            
+            <Button onClick={test}>
+                click
+            </Button>
             <Link to="/Upload/Meme" className="Signup-link">
                 <Button appearance="ghost">Upload memes</Button>
             </Link>
