29ec85d1ba8caf86631ad734b52d1190f87296b3
oeje <oeje@itu.dk>
2020-10-07 17:14:32 +0200

    rendering of random memes work, added outline to text, rezized both canvas and image based on viewport and image size, only needs static button placements

--- frontend/src/MemePage.tsx
+++ frontend/src/MemePage.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from 'rsuite';
+import { threadId } from 'worker_threads';
 import { useMemeState } from './State';
 
 var fontBase = 1000,                   // selected default width for canvas
@@ -14,43 +15,80 @@ function getFont(width:number) {
 
 function drawText(ctx:CanvasRenderingContext2D,text:string,centerX:number,centerY:number,font:string){
     ctx.save();
+    ctx.font = font;
     ctx.textAlign='center';
     ctx.textBaseline='middle';
+    ctx.fillStyle = "white";
+    ctx.strokeStyle='black';
     ctx.fillText(text,centerX,centerY);
+    ctx.strokeText(text,centerX,centerY)
     ctx.restore();
+}
+
+function getWindowDimensions() {
+    const { innerWidth: width, innerHeight: height } = window;
+    return {
+      width,
+      height
+    };
+  }
+  
+export function useWindowDimensions() {
+    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+  
+    useEffect(() => {
+      function handleResize() {
+        setWindowDimensions(getWindowDimensions());
+      }
+  
+      window.addEventListener('resize', handleResize);
+      return () => window.removeEventListener('resize', handleResize);
+    }, []);
+  
+    return windowDimensions;
 }
 
 const MemePage :React.FC = (props) =>{
     const canvasRef = React.useRef<HTMLCanvasElement>(null);
     
-    const CHANCE_OF_TOPTEXT = 25;
-    const CHANCE_OF_SOUND = 50;
-    const chance_OF_BOTTOMTEXT = 25;
+    const CHANCE_OF_TOPTEXT = 50;
+    const CHANCE_OF_SOUND = 25;
+    const chance_OF_BOTTOMTEXT = 50;
     
     const [memeState,setMemeState] = useState({toptext:"",bottomtext:"",visualFileURL:"",soundFileURL:""})
-
+    const windowDimensions = useWindowDimensions();
     useEffect(() => {
         var imageElement = new Image();
         imageElement.src = memeState.visualFileURL
         imageElement.addEventListener('load', () => {
             var memeCanvas = canvasRef.current;
-            console.log("hello")
-            console.log(memeState.visualFileURL)
             if(memeCanvas){
                 const ctx = memeCanvas.getContext('2d') 
                 if (ctx){
-                    //Our first draw
-                    memeCanvas.width = imageElement.width;
-                    memeCanvas.height = imageElement.height;
-                    ctx.drawImage(imageElement,0,0);
+                    
+
+                    let maxDimension = windowDimensions.width < 400 ? windowDimensions.width - 50 : 400;
+                    
+                    if (imageElement.width > maxDimension && imageElement.width >= imageElement.height){
+                        memeCanvas.width = maxDimension;
+                        memeCanvas.height = maxDimension * (imageElement.height/imageElement.width);
+                    }else if (imageElement.height > maxDimension && imageElement.height > imageElement.width){
+                        memeCanvas.height = maxDimension;
+                        memeCanvas.width = maxDimension * (imageElement.width/imageElement.height);
+                    }else {
+                        memeCanvas.width = imageElement.width;
+                        memeCanvas.height = imageElement.height;
+                    }
+                   
+                    var scale = Math.min(memeCanvas.width / imageElement.width, memeCanvas.height / imageElement.height);
+                    // get the top left position of the image
+                    var x = (memeCanvas.width / 2) - (imageElement.width / 2) * scale;
+                    var y = (memeCanvas.height / 2) - (imageElement.height / 2) * scale;
+                    ctx.drawImage(imageElement, x, y, imageElement.width * scale, imageElement.height * scale);
                     let font = getFont(memeCanvas.width);
 
-                    ctx.font = font;
-                    ctx.textAlign = "center";
-                    ctx.fillStyle = "black";
-
-                    drawText(ctx,memeState.toptext,memeCanvas.width/2, - memeCanvas.height/5,font);
-                    drawText(ctx,memeState.bottomtext,memeCanvas.width/2, memeCanvas.height - memeCanvas.height/5,font);                   
+                    drawText(ctx,memeState.toptext,memeCanvas.width/2, memeCanvas.height/8,font);
+                    drawText(ctx,memeState.bottomtext,memeCanvas.width/2, memeCanvas.height - memeCanvas.height/8,font);                   
                 }
             }
         })
