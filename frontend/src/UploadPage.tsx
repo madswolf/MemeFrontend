99c1327893e11dd20c30d3688f8eef24e41d9f96
MadsWJespersen <Mads_1997@live.dk>
2020-10-08 17:22:15 +0200

    added user feedback for when a button is too large

--- frontend/src/UploadPage.tsx
+++ frontend/src/UploadPage.tsx
@@ -1,26 +1,36 @@
 import React, { useEffect, useState } from 'react';
-import { Button, ButtonToolbar, ControlLabel, Form, FormControl, FormGroup, HelpBlock, Input, Loader, Schema } from 'rsuite';
+import { Button, ButtonToolbar, ControlLabel, Form, FormControl, FormGroup, HelpBlock, Input, Loader, Schema, Tooltip, Whisper } from 'rsuite';
 import { useMemeState } from './State';
 import axios from 'axios';
 import { MemeCanvas } from './MemeCanvas';
+import ReactTooltip from 'react-tooltip';
 
 const UploadPage :React.FC = (props) =>{
   const {toptext,setTopText,bottomtext,setBottomText,visualFile,setVisualFile,soundFile,setSoundFile} = useMemeState();
   const [isLoading,setIsLoading] = useState(false);
   const [visualFileURL,setVisualFileURL] = useState("");
   const [soundFIleURL,setSoundFileURL] = useState("");
+  const [readyToUpload, setReadyToUpload] = useState(false);
+  const [toolTipString,setToolTipString] = useState("");
 
   //hacky way to force rerender since form.reset does not work
   const [reset,setReset] = useState("reset");
 
   useEffect(() => {
-    const thing = document.getElementById('submit') as HTMLButtonElement;
     if (!visualFile) {
-      thing.disabled = true;
+      setReadyToUpload(false);
+      setToolTipString("");
     }else {
-      thing.disabled = false;
+      if(visualFile.size > 10000000){
+        setReadyToUpload(false);
+        setToolTipString("File is too large");
+      } else {
+        setReadyToUpload(true);
+        setToolTipString("");
+      }
     }
-  })
+    ReactTooltip.rebuild();
+  });
 
   function handleUpload(){
     if (visualFile){
@@ -31,7 +41,6 @@ const UploadPage :React.FC = (props) =>{
       if(soundFile){
         formdata.append("soundFile",soundFile);
       }
-      console.log(window.location.href +'s')
       setIsLoading(true);
       axios.post(window.location.href +'s',formdata, {
         headers: {
@@ -71,11 +80,25 @@ const UploadPage :React.FC = (props) =>{
     }
   }
 
+  const UpLoadButton:React.FC<{toolTopString:string,readyForUpload:boolean}> =(props) => {
+    var upLoadButton;
+    if (toolTipString){
+      upLoadButton = (<Button disabled data-tip data-for="submit" block appearance="primary" onClick={handleUpload}>Upload</Button>  );
+    }else {
+      if(!readyToUpload){
+        upLoadButton = (<Button disabled  block appearance="primary" onClick={handleUpload}>Upload</Button>);
+      } else {
+        upLoadButton = (<Button block appearance="primary" onClick={handleUpload}>Upload</Button>);
+      } 
+    }
+    return (upLoadButton);
+  }
+
   const MemeLoader:React.FC<{isloading:boolean}> = (props) => {
     const loader = props.isloading ? (<Loader backdrop content="sending..." vertical/>) : <div></div>
     return (loader);
   }
-
+  ReactTooltip.hide();
   return (
     <div key={reset} className="Upload-page">
       <Form className="Login-form">
@@ -99,12 +122,15 @@ const UploadPage :React.FC = (props) =>{
           </FormGroup>
           <FormGroup>
             <ButtonToolbar>
-              <Button block id="submit" appearance="primary" onClick={handleUpload}>Upload</Button>  
+              <UpLoadButton toolTopString={toolTipString} readyForUpload={readyToUpload}/>
             </ButtonToolbar>
           </FormGroup>
           <MemeLoader isloading={isLoading}/>
       </Form>
       <MemeCanvas className="Meme-preview-container" memeState={{toptext:toptext,bottomtext:bottomtext,visualFileURL:visualFileURL,soundFileURL:soundFIleURL}}/>
+      <ReactTooltip id="submit" place="top" effect="solid">
+        {toolTipString}
+      </ReactTooltip>
     </div>
     );
 }
