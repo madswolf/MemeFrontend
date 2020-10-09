e01bc5f9bb339048cb97d1e80a50ce9b6bd52b96
oeje <oeje@itu.dk>
2020-10-09 02:12:00 +0200

    added debug statements

--- server/src/index.ts
+++ server/src/index.ts
@@ -43,8 +43,11 @@ createConnection().then(async connection => {
         (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
             const result = (new (route.controller as any))[route.action](req, res, next);
             if (result instanceof Promise) {
-                result.then(result => result !== null && result !== undefined ? res.send(result) : undefined)
-
+                result.then(result => {
+                    console.log(result);    
+                    result !== null && result !== undefined ? res.send(result) : undefined;
+                    }
+                );
             } else if (result !== null && result !== undefined) {
                 res.json(result)
             }
