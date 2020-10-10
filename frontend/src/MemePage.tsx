e4caa65cf61394b9ba5609e03fd150e2ed23256a
oeje <oeje@itu.dk>
2020-10-10 17:59:06 +0200

    made a meme stack so that you can go back and forth between new and old memes

--- frontend/src/MemePage.tsx
+++ frontend/src/MemePage.tsx
@@ -1,16 +1,35 @@
-import React, { useEffect, useState } from 'react';
+import React, { ReactElement, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Button } from 'rsuite';
+import { Button, Icon, IconButton, IconButtonProps } from 'rsuite';
+import { JsxElement } from 'typescript';
 import { MemeCanvas } from './MemeCanvas';
-import { useMemeCanvasState } from './State';
+import { useMemeCanvasState, useMemeStackState } from './State';
 
+const MemeControleButton : React.FC<{isAllowed:boolean,className:string,isRight:boolean,onClick():void}> = (props) => {
+    const iconToUse = <Icon icon={props.isRight ? "arrow-right" : "arrow-left"} />; 
+    
+    var button;
+    if(props.isAllowed){
+        button =  
+        <IconButton className={props.className} icon ={iconToUse} appearance="primary" onClick={props.onClick} >
+            {props.isRight ? "forward" : "back"}
+        </IconButton>
+    } else {
+        button =  
+        <IconButton className={props.className} disabled icon ={iconToUse} appearance="primary" onClick={props.onClick} placement={props.isRight ? "right" : "left"}>
+             {props.isRight ? "forward" : "back"}
+        </IconButton>
+    }
+    return button;
+}
 
 const MemePage :React.FC = (props) =>{
     
     const CHANCE_OF_TOPTEXT = 75;
     const CHANCE_OF_SOUND = 25;
     const chance_OF_BOTTOMTEXT = 75;
-    const {memeState,setMemeState} = useMemeCanvasState();
+    //const {memeState,setMemeState} = useMemeCanvasState();
+    const {memeState,memeStackPointer,canGoBack,canGoForward,append,goBack,goForward} = useMemeStackState();
 
     async function getResourceOnChance(fetchURL:string,chance:number):Promise<string>{
         if( Math.floor(Math.random() * 100) < chance){
@@ -23,13 +42,12 @@ const MemePage :React.FC = (props) =>{
     }
 
     async function getRandom(){
-        console.log(window.location.href)
-        
         const visualResource = await getResourceOnChance(`${window.location.href}/random/visual`,100);
         const soundResource = await getResourceOnChance(`${window.location.href}/random/sound`,CHANCE_OF_SOUND);
         const toptextResource = await getResourceOnChance(`${window.location.href}/random/toptext`,CHANCE_OF_TOPTEXT);
         const bottomtextResource = await getResourceOnChance(`${window.location.href}/random/bottomtext`,chance_OF_BOTTOMTEXT);
-        setMemeState({toptext:toptextResource,bottomtext:bottomtextResource,visualFileURL:visualResource,soundFileURL:soundResource})
+        append({toptext:toptextResource,bottomtext:bottomtextResource,visualFileURL:visualResource,soundFileURL:soundResource})
+    
     }
     return (
         <div>
@@ -42,9 +60,14 @@ const MemePage :React.FC = (props) =>{
                 </h3>
             </div>
                 <MemeCanvas className="Meme-container" memeState={memeState} >
-                    <Button appearance="primary" onClick={getRandom}>
-                        New meme
-                    </Button>
+                    <div className="Meme-button-container">
+                        <MemeControleButton className="Meme-button" isAllowed={canGoBack} isRight={false} onClick={goBack} />
+                        <Button className="Meme-button" appearance="primary" onClick={getRandom}>
+                            New meme
+                        </Button>    
+                        <MemeControleButton className="Meme-button" isAllowed={canGoForward} isRight={true} onClick={goForward} />
+                    </div>
+                    <h1>{memeStackPointer}</h1>
                 </MemeCanvas>
         </div>
     );
