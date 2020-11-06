6e0e19b9a6279e9a4193fa2d6d8b3b5ba1a13d51
MadsWJespersen <Mads_1997@live.dk>
2020-11-06 16:39:50 +0000

    minor deployement tweaks

--- server/src/routes/UserRoutes.ts
+++ server/src/routes/UserRoutes.ts
@@ -1,15 +1,15 @@
 import { Router } from "express";
 import UserController from "../controllers/UserController";
-import { checkJwt } from "../middlewares/checkJwt";
+import { checkJWT } from "../middlewares/checkjwt";
 import { checkRole } from "../middlewares/checkRole";
 
 const router = Router();
 
-router.get("/", [checkJwt, checkRole(["ADMIN"])], UserController.all);
+router.get("/", [checkJWT, checkRole(["ADMIN"])], UserController.all);
 
 router.get(
   "/:id([0-9]+)",
-  [checkJwt, checkRole(["ADMIN"])],
+  [checkJWT, checkRole(["ADMIN"])],
   UserController.one
 );
 
@@ -17,19 +17,19 @@ router.post("/signup", UserController.save);
 
 router.patch(
   "/:id([0-9]+)",
-  [checkJwt, checkRole(["ADMIN"])],
+  [checkJWT, checkRole(["ADMIN"])],
   UserController.updateRole
 );
 
 router.delete(
   "/:id([0-9]+)",
-  [checkJwt, checkRole(["ADMIN"])],
+  [checkJWT, checkRole(["ADMIN"])],
   UserController.remove
 );
 
 router.post("/login", UserController.login);
 
-router.post("/update",[checkJwt], UserController.update);
+router.post("/update",[checkJWT], UserController.update);
 
 router.post("/forgot-password", UserController.recoverPassword);
 
