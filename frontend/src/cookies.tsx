28cfc7f0e707a7d52e0ed0eb58e768cec48afd9f
MadsWJespersen <Mads_1997@live.dk>
2021-02-23 02:08:02 +0100

    made the inputs sliders

--- frontend/src/cookies.tsx
+++ frontend/src/cookies.tsx
@@ -13,7 +13,7 @@ export function getCookie(name: string) {
     const value = "; " + document.cookie;
     const parts = value.split("; " + name + "=");
     
-    if (parts.length == 2) {
+    if (parts.length === 2) {
         // @ts-ignore
         return parts.pop().split(";").shift();
     }
